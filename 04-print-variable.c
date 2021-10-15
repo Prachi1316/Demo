bf9324708769bc037bd0c7c75ad50f16ca850cdb
Prachi1316 <90784918+Prachi1316@users.noreply.github.com>
2021-10-15 14:16:58 +0530

    Add files via upload

---
+++ 04-print-variable.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+
+    //print variables
+    int a = 120, b = 55;
+    char c = 'x';
+    float f = 120.6;
+
+    printf("\n\n");
+
+    printf("value of a is %d & b is %d\n", a, b);
+    printf("value of c is %c\n", c);
+    printf("value of f is %f\n", f);
+
+    printf("\n");
+
+    printf("a=%c\n", a); // ASCII(we can convert value of character into integer and integer into character)
+    printf("c=%d\n", c);
+
+    printf("f is %c\n", f); // not applicable or not convertable
+    printf(" f is %d", f);
+}
+
+
+
+
